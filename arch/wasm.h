18d9815458c305d6f5aecdeb5a2ea8ccbb6a5e43
Karel Bilek <kb@karelbilek.com>
2024-02-25 22:20:27 +0100

    Update wasm.h

--- arch/wasm.h
+++ arch/wasm.h
@@ -1,20 +1,26 @@
-/* Copyright (C) 2001-2018 Artifex Software, Inc.
+/* Copyright (C) 2001-2012 Artifex Software, Inc.
    All Rights Reserved.
+
    This software is provided AS-IS with no warranty, either express or
    implied.
+
    This software is distributed under license and may not be copied,
    modified or distributed except as expressly authorized under the terms
    of the license contained in the file LICENSE in this distribution.
+
    Refer to licensing information at http://www.artifex.com or contact
-   Artifex Software, Inc.,  1305 Grant Avenue - Suite 200, Novato,
-   CA 94945, U.S.A., +1(415)492-9861, for further information.
+   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
+   CA  94903, U.S.A., +1(415)492-9861, for further information.
 */
+
+// copied from windows, with ptr_mod and sizeof_ptr changes
 
 	 /* ---------------- Scalar alignments ---------------- */
 
 #define ARCH_ALIGN_SHORT_MOD 2
 #define ARCH_ALIGN_INT_MOD 4
 #define ARCH_ALIGN_LONG_MOD 4
+// wasm change
 #define ARCH_ALIGN_PTR_MOD 4
 #define ARCH_ALIGN_FLOAT_MOD 4
 #define ARCH_ALIGN_DOUBLE_MOD 8
@@ -25,11 +31,8 @@
 #define ARCH_LOG2_SIZEOF_SHORT 1
 #define ARCH_LOG2_SIZEOF_INT 2
 #define ARCH_LOG2_SIZEOF_LONG 2
-
-#ifndef ARCH_SIZEOF_GX_COLOR_INDEX
 #define ARCH_SIZEOF_GX_COLOR_INDEX 8
-#endif
-
+// wasm change
 #define ARCH_SIZEOF_PTR 4
 #define ARCH_SIZEOF_FLOAT 4
 #define ARCH_SIZEOF_DOUBLE 8
@@ -50,6 +53,3 @@
 #define ARCH_FLOATS_ARE_IEEE 1
 #define ARCH_ARITH_RSHIFT 2
 #define ARCH_DIV_NEG_POS_TRUNCATES 1
-
-#define ARCH_SIZEOF_SIZE_T 4
-#define ARCH_MAX_SIZE_T ((unsigned int)~0 + (unsigned int)0)
